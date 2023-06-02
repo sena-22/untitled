e858df0ede50c5d041130b522fbdeb467c2f4270
sena-22 <senika_@naver.com>
2023-06-02 21:51:44 +0900

    :art: Style: prettier, eslint, husky 설정
    Related to: #1

---
+++ .prettierrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  singleQuote: true,
+  tabWidth: 2,
+  printWidth: 100,
+  semi: true,
+  arrowParens: 'avoid',
+  bracketSameLine: false,
+  trailingComma: 'all',
+  useTabs: false,
+};
